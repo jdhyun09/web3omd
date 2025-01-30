b2dd63fe35f29d66f7a72ab7845d0892b00fd042
jdhyun09 <jdhyun09@naver.com>
2025-01-30 18:47:28 +0900

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { sepolia } from 'wagmi/chains';
+import { Toaster } from 'react-hot-toast';
 
-import { config } from '../wagmi';
+const config = getDefaultConfig({
+  appName: 'Community Gallery',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [sepolia],
+});
 
 const client = new QueryClient();
 
@@ -16,6 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <QueryClientProvider client={client}>
         <RainbowKitProvider>
           <Component {...pageProps} />
+          <Toaster position="bottom-right" />
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
